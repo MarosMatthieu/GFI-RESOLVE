10abe735dfd5193a9b1a12023a99ce0df54437fa
Erwan <Erwan@DESKTOP-1MNAJL1>
2017-10-04 09:36:39 +0200

    Login_page

---
+++ Login_page/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title> LOGIN PAGE</title>
+	</head>
+	<body>
+		<section class="logo">
+		<img src="images/logo.jpg" />
+		</section>
+		
+		<section class="formulaire">
+			<div class="form">
+			<form method="post" action="traitement.php">
+				<input type="text" name="Adresse mail" placeholder="login"/>
+			</form>
+			<br/>
+			<form method="post" action="traitement.php">
+				<input type="password" name="Mot de Passe" placeholder="Password"/>
+			</form>
+			<br/>
+		
+			<input class="envoyer" type="submit" value="Envoyer"> 
+			</div>
+		</section>
+		
+		<footer>
+		<p> </p>
+		</footer>
+	
+	</body>
+	
+	
+	
+</html>
