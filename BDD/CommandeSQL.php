9f3aa2aab97a94c57f6a6b665094d0555b96d130
Matthieu Maros <Matthieu Maros@DESKTOP-LD1QH2M>
2017-10-04 12:14:10 +0200

    Ajout d'un fichier pour les demande de resolution de requette SQL

---
+++ BDD/CommandeSQL.php
@@ -0,0 +1,7 @@
+Example:
+- Je voudrais inserer un contact en fonction de son entre prise
+- Je voudrais ajouter des clients
+- Je voudrais ajouter une prestation en fonction d'un client et d'un commercial.
+
+
+Vos demandes : 
